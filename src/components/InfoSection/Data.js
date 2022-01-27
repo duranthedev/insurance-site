e6611593f0edb7635b28ddc48d38af14600f9925
duranthedev <duranthedev@gmail.com>
2022-01-26 18:16:50 -0600

    Updated main banner & 1st draft of second section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Term Life Insurance',
+  headline: 'Do I even need life insurance?',
+  description:
+    'If anyone is depending on your income or if you have obligations (debt, mortgage, etc.) that would fall to someone else to handle if you were to die, then the answer is, "Yes."',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description:
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../images/svg-2.svg'),
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Creating an account is extremely easy',
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg'),
+  alt: 'Papers',
+  dark: false,
+  primary: false,
+  darkText: true
+};
